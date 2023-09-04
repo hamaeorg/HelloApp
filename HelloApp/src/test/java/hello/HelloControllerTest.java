37c31c9bbceede6b97cba65b8796ed38e7edcae0
hamaeorg <hannes.maeorg@gmail.com>
2023-09-04 17:19:42 +0300

    webhook test

--- HelloApp/src/test/java/hello/HelloControllerTest.java
+++ HelloApp/src/test/java/hello/HelloControllerTest.java
@@ -25,7 +25,7 @@ public class HelloControllerTest {
 		mvc.perform(MockMvcRequestBuilders.get("/")
                 .accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Hello aws jenkins")));
+				.andExpect(content().string(equalTo("Hello")));
 	}
 }
 
